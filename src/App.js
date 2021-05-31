2e8dcf01cc8bba3c4e79b08e84c2a1e8e85faba4
ritik723 <ritiksingh7239@gmail.com>
2021-05-31 19:59:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'resuite/dist/styles/rsuite-default.css'
+function App() {
+  return (
+    <div>
+      hello
+      
+    </div>
+  );
+}
+
+export default App;
