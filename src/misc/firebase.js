1e5dba6053dbfd9194e653ea320ff3ab0e435e70
ritik723 <ritiksingh7239@gmail.com>
2021-06-02 13:24:30 +0530

    added SignIn feature through Google/Facebook

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,7 @@
-import firbase from 'firebase/app';
+import firebase from 'firebase/app';
+import 'firebase/auth'; 
+import 'firebase/database';
+
 
 const config ={
     apiKey: "AIzaSyCphLqgJYo_8rcGvSfy3rmjHSllagjwR5M",
@@ -11,5 +14,7 @@ const config ={
   };
 
   const app = firebase.initializeApp(config);
+  export const auth = app.auth();
+  export const database = app.database();
 
   
