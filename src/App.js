7c413434a0fb49485813b65dc9e05b3903fb5c89
ritik723 <ritiksingh7239@gmail.com>
2021-06-02 11:32:24 +0530

    created public and private route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import 'resuite/dist/styles/rsuite-default.css'
+import React from "react";
+import "rsuite/dist/styles/rsuite-default.css";
+import "./styles/main.scss";
+import { Switch } from "react-router";
+import SignIn from "./pages/SignIn";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
+import PublicRoute from './components/PublicRoute';
+
 function App() {
   return (
-    <div>
-      hello
-      
-    </div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
 export default App;
- 
---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+import firbase from 'firebase/app';
+
+const config ={
+    apiKey: "AIzaSyCphLqgJYo_8rcGvSfy3rmjHSllagjwR5M",
+    authDomain: "chat-web-app-c7a83.firebaseapp.com",
+    databaseURL: "https://chat-web-app-c7a83-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-web-app-c7a83",
+    storageBucket: "chat-web-app-c7a83.appspot.com",
+    messagingSenderId: "162726332294",
+    appId: "1:162726332294:web:817674f14a0c704692ce07"
+  };
+
+  const app = firebase.initializeApp(config);
+
+  
