63b27d764085acaa5afbf6fd5e7b37ddcbca5f23
ritik723 <ritiksingh7239@gmail.com>
2021-06-11 20:11:55 +0530

    added context romms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Grid,Row,Col, } from 'rsuite';
 import Sidebar from '../components/Sidebar';
+import { RoomsProvider } from '../context/rooms.context';
   
 
 const Home = () => {
     return (
+        <RoomsProvider>
         <Grid fluid className="h-100">
             <Row className="h-100">
                 <Col xs={24} md={8} className="h-100">
@@ -13,6 +15,7 @@ const Home = () => {
             </Row>
 
         </Grid>
+        </RoomsProvider>
     );
 };
 
