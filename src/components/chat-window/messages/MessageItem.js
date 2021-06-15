4d81c5f78c31efff5e35559d3a3be2d6fa3a40d0
ritik723 <ritiksingh7239@gmail.com>
2021-06-15 15:23:31 +0530

    displayed user data in with model window

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
+import ProfileInfoBtnModal from '../messages/ProfileInfoBtnModal'
 
 const MessageItem = ({message}) => {
     const {author,createdAt , text } = message;
     return  <li className="padded mb-1">
     <div className="d-flex align-items-center font-bolder mb-1">
         <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="xs" />
-        <span className="ml-2">{author.name}</span>
+    
+        <ProfileInfoBtnModal profile={author} appearance="link" className="p-0 ml-1 text-black" />
         <TimeAgo
          datetime={createdAt}
          className="font-normal text-black-45 ml-2" />
