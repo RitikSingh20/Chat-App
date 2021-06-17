71c463cf30eef82d5c3be7aa98f43942be513ca0
ritik723 <ritiksingh7239@gmail.com>
2021-06-17 12:55:30 +0530

    added button to grant or remove admin acces

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,26 +1,60 @@
-import React from 'react'
-import TimeAgo from 'timeago-react';
-import ProfileAvatar from '../../dashboard/ProfileAvatar';
-import ProfileInfoBtnModal from '../messages/ProfileInfoBtnModal'
-import PresenceDot from '../../PresenceDot';
+import React, {memo} from "react";
+import TimeAgo from "timeago-react";
+import ProfileAvatar from "../../dashboard/ProfileAvatar";
+import ProfileInfoBtnModal from "../messages/ProfileInfoBtnModal";
+import PresenceDot from "../../PresenceDot";
+import { CurrentRoomProvider, useCurrentRoom } from "../../../context/current-room.context";
+import { Button } from "rsuite";
+import { auth } from "../../../misc/firebase";
 
-const MessageItem = ({message}) => {
-    const {author,createdAt , text } = message;
-    return  <li className="padded mb-1">
-    <div className="d-flex align-items-center font-bolder mb-1">
+const MessageItem = ({ message, handleAdmin}) => {
+  const { author, createdAt, text } = message;
+
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admins= useCurrentRoom( v=> v.admins);
+
+const isMsgAuthorAdmin = admins.includes(author.uid);
+const isAuthor = auth.currentUser.uid === author.uid;
+const canGrantAdmin = isAdmin && !isAuthor;
+
+
+
+  return (
+    <li className="padded mb-1">
+      <div className="d-flex align-items-center font-bolder mb-1">
         <PresenceDot uid={author.uid} />
-        <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="xs" />
-    
-        <ProfileInfoBtnModal profile={author} appearance="link" className="p-0 ml-1 text-black" />
-        <TimeAgo
-         datetime={createdAt}
-         className="font-normal text-black-45 ml-2" />
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
 
-    </div>
-    <div>
-    <span className="word-breal-all">{text}</span>
-    </div>
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        >
+        
+        {canGrantAdmin && 
+        <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+            {isMsgAuthorAdmin ? 'Remove admin permission': 'Give admin in this room'}
+
+        </Button>
+         }
+         </ProfileInfoBtnModal>
+
+
+        <TimeAgo
+          datetime={createdAt}
+          className="font-normal text-black-45 ml-2"
+        />
+      </div>
+      <div>
+        <span className="word-breal-all">{text}</span>
+      </div>
     </li>
+  );
 };
 
-export default MessageItem;
+export default memo(MessageItem);
