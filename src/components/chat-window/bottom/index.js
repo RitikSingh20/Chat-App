27d9d8d4682fb7f7dab75b813d7860057b1c1347
ritik723 <ritiksingh7239@gmail.com>
2021-06-19 13:11:02 +0530

    added audio recording feautre

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router';
 import { useProfile } from '../../../context/profile.context';
 import { database } from '../../../misc/firebase';
 import AttachmentBtnModal from './AttachmentBtnModal';
+import AudioMsgBtn from '../bottom/AudioMsgBtn'
  
 
 function assembleMessage(profile, chatId) {
@@ -106,6 +107,7 @@ const Bottom = () => {
     <div>
       <InputGroup>
         <AttachmentBtnModal afterUpload={afterUpload} />
+        <AudioMsgBtn afterUpload={afterUpload}/>
        
         <Input
           placeholder="Write a new message here..."
