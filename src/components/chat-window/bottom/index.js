8cb13d6b0ec5eae4532e696f9c297ad1f5ee8147
ritik723 <ritiksingh7239@gmail.com>
2021-06-19 09:39:43 +0530

    uploded files to database

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,11 +1,11 @@
-import React, { useState, useCallback } from "react";
-import { InputGroup, Input, Icon } from "rsuite";
-import firebase from "firebase/app";
-import { useParams } from "react-router";
-import { Alert } from "rsuite";
-import { database } from "../../../misc/firebase";
-import { useProfile } from "../../../context/profile.context";
-import AttachmentBtnModal from "./AttachmentBtnModal";
+import React, { useState, useCallback } from 'react';
+import { InputGroup, Input, Icon, Alert } from 'rsuite';
+import firebase from 'firebase/app';
+import { useParams } from 'react-router';
+import { useProfile } from '../../../context/profile.context';
+import { database } from '../../../misc/firebase';
+import AttachmentBtnModal from './AttachmentBtnModal';
+ 
 
 function assembleMessage(profile, chatId) {
   return {
@@ -22,62 +22,98 @@ function assembleMessage(profile, chatId) {
 }
 
 const Bottom = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const { chatId } = useParams();
-
   const { profile } = useProfile();
 
-  const onInputChange = useCallback((value) => {
+  const onInputChange = useCallback(value => {
     setInput(value);
   }, []);
 
   const onSendClick = async () => {
-    if (input.trim() === "") {
+    if (input.trim() === '') {
       return;
     }
+
     const msgData = assembleMessage(profile, chatId);
     msgData.text = input;
 
     const updates = {};
-    const messageId = database.ref("messages").push().key;
+
+    const messageId = database.ref('messages').push().key;
 
     updates[`/messages/${messageId}`] = msgData;
     updates[`/rooms/${chatId}/lastMessage`] = {
       ...msgData,
       msgId: messageId,
     };
+
     setIsLoading(true);
     try {
       await database.ref().update(updates);
 
-      setInput("");
+      setInput('');
       setIsLoading(false);
     } catch (err) {
       setIsLoading(false);
-
       Alert.error(err.message);
     }
   };
 
-  const onKeyDown =(ev) => {
-      if(ev.keyCode === 13){
-          ev.preventDefault();
-          onSendClick();
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      ev.preventDefault();
+      onSendClick();
+    }
+  };
+
+  const afterUpload = useCallback(
+    async files => {
+      setIsLoading(true);
+
+      const updates = {};
+
+      files.forEach(file => {
+        const msgData = assembleMessage(profile, chatId);
+        msgData.file = file;
+
+        const messageId = database.ref('messages').push().key;
+
+        updates[`/messages/${messageId}`] = msgData;
+      });
+
+      const lastMsgId = Object.keys(updates).pop();
+
+      updates[`/rooms/${chatId}/lastMessage`] = {
+        ...updates[lastMsgId],
+        msgId: lastMsgId,
+      };
+
+      try {
+        await database.ref().update(updates);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        Alert.error(err.message);
       }
-  }
+    },
+    [chatId,profile]
+  );
 
   return (
     <div>
       <InputGroup>
-      <AttachmentBtnModal/>
+        <AttachmentBtnModal afterUpload={afterUpload} />
+       
         <Input
           placeholder="Write a new message here..."
           value={input}
           onChange={onInputChange}
           onKeyDown={onKeyDown}
         />
+
         <InputGroup.Button
           color="blue"
           appearance="primary"
