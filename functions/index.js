03ccdf4fea22cfda94f14358d5cb3d7ca5213228
ritik723 <ritiksingh7239@gmail.com>
2021-06-21 15:21:23 +0530

    added cloud function to send fcm  message

--- functions/index.js
+++ functions/index.js
@@ -11,10 +11,6 @@ admin.initializeApp({
 });
 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const {sendFcm} = require('./src/fcm');
+
+exports.sendFcm = sendFcm;
